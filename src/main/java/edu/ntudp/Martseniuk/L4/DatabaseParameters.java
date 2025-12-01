40f7fe32204914d358f9799d802dd335b1ab9113
Hanna <hanna@gmail.com>
2025-12-01 13:33:17 +0200

    First Tru Git Push

---
+++ src/main/java/edu/ntudp/Martseniuk/L4/DatabaseParameters.java
@@ -0,0 +1,7 @@
+package edu.ntudp.Martseniuk.L4;
+
+public class DatabaseParameters {
+    public static final String url = "jdbc:h2:~/test;AUTO_SERVER=TRUE";
+    public static final String user = "sa";
+    public static final String password = "";
+}
